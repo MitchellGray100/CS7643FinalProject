190c4b2ef25ac6be12131b83b5aabddef9508c2f
Paul Li <yli3850@gatech.edu>
2025-11-18 19:59:24 -0500

    updated loader: precompute point cloud cache

--- point_pillar/modelnet_dataset.py
+++ point_pillar/modelnet_dataset.py
@@ -18,115 +18,156 @@ from torch.utils.data import Dataset
 
 # only to verify the files
 def read_off(path):
-  """
-  Read .off file and return a raw vertices np array (float32) and a faces np array (int16)
-  """
-  with open(path, 'r') as f:
-    # First row is always "OFF"
-    header = f.readline().strip()
-    assert header == 'OFF'
+	"""
+	Read .off file and return a raw vertices np array (float32) and a faces np array (int16)
+	"""
+	with open(path, 'r') as f:
+		# First row is always "OFF"
+		header = f.readline().strip()
+		assert header == 'OFF'
 
-    # Second row is 
-    # numVertices numFaces numEdges
-    counts = f.readline().strip().split()
-    n_verts = int(counts[0])
-    n_faces = int(counts[1])
+		# Second row is 
+		# numVertices numFaces numEdges
+		counts = f.readline().strip().split()
+		n_verts = int(counts[0])
+		n_faces = int(counts[1])
 
-    # next are verts
-    verts = []
-    for _ in range(n_verts):
-      line = f.readline()
-      if not line:
-        break
-      x, y, z = map(float, line.strip().split()[:3])
-      verts.append([x, y, z])
+		# next are verts
+		verts = []
+		for _ in range(n_verts):
+			line = f.readline()
+			if not line:
+				break
+			x, y, z = map(float, line.strip().split()[:3])
+			verts.append([x, y, z])
 
-    # the rest are faces.
-    faces = []
-    for _ in range(n_faces):
-      line = f.readline()
-      if not line:
-        break
-      n, a, b, c = map(int, line.strip().split()[:4])
-      faces.append([n, a, b, c])
+		# the rest are faces.
+		faces = []
+		for _ in range(n_faces):
+			line = f.readline()
+			if not line:
+				break
+			n, a, b, c = map(int, line.strip().split()[:4])
+			faces.append([n, a, b, c])
 
-  return np.asarray(verts, dtype=np.float32), np.asarray(faces, dtype=np.int16)
+	return np.asarray(verts, dtype=np.float32), np.asarray(faces, dtype=np.int16)
 
 def sample_off_surface(path, n_points: int=2048):
-  """
-  using trimesh to read an OFF file and sample the surfaces. return np array float32
-  """
-  mesh = trimesh.load(path, process=True)
-  points, _ = trimesh.sample.sample_surface(mesh, n_points)
+	"""
+	using trimesh to read an OFF file and sample the surfaces. return np array float32
+	"""
+	mesh = trimesh.load(path, process=True)
+	points, _ = trimesh.sample.sample_surface(mesh, n_points)
 
-  return points.astype(np.float32)
+	return points.astype(np.float32)
 
 def normalize_point_cloud(points: np.ndarray) -> np.ndarray:
-  """
-  center and scale the point cloud
-  """
-  centroid = np.mean(points, axis=0)
-  points = points - centroid
-  max_dist = np.max(np.linalg.norm(points, axis=1))
-  points = points / max_dist
+	"""
+	center and scale the point cloud
+	"""
+	centroid = np.mean(points, axis=0)
+	points = points - centroid
+	max_dist = np.max(np.linalg.norm(points, axis=1))
+	points = points / max_dist
 
-  return points.astype(np.float32)
-  
+	return points.astype(np.float32)
+	
 
 
 class ModelNetDataset(Dataset):
-  """
-  pytorch dataset for ModelNet10 and ModelNet40.
+	"""
+	pytorch dataset for ModelNet10 and ModelNet40.
 
-  expected folder structure:
-  root/
-    class1/
-        train/
-            *.off
-        test/
-            *.off
-    class2/
-    ...
+	expected folder structure:
+	root/
+		class1/
+				train/
+						*.off
+				test/
+						*.off
+		class2/
+		...
 
-  To use Dataset class:
-    from torch.utils.data import DataLoader
-    train_dataset = ModelNetDataset(root, 'train')
-    train_loader = DataLoader(train_dataset, batch_size)
-  """
-  def __init__(self, root:str, split:str='train', num_points:int=2048, normalize:bool=True, ):
-    super().__init__()
-    self.root = root
-    self.num_points = num_points
-    self.normalize = normalize
-    self.split = split
+	To use Dataset class:
+		from torch.utils.data import DataLoader
+		train_dataset = ModelNetDataset(root, 'train')
+		train_loader = DataLoader(train_dataset, batch_size)
+	"""
+	def __init__(
+			self, root:str, split:str='train', num_points:int=2048, normalize:bool=True, 
+			precomputed_root: str | None = None,
+			cache_mode: str = "none",
+			):
+		super().__init__()
+		self.root = root
+		self.num_points = num_points
+		self.normalize = normalize
+		self.split = split
 
-    class_dirs = sorted(
-              d for d in os.listdir(self.root)
-              if os.path.isdir(os.path.join(self.root, d))
-          )
-    self.class_names = class_dirs
-    self.class_to_idx = {cls: i for i, cls in enumerate(self.class_names)}
+		self.precomputed_root = precomputed_root
+		self.cache_mode = cache_mode
 
-    self.samples = []
-    for cls in self.class_names:
-      split_dir = os.path.join(self.root, cls, self.split)
-      off_files = glob.glob(os.path.join(split_dir, "*.off"))
-      for fpath in off_files:
-        self.samples.append((fpath, self.class_to_idx[cls]))
+		class_dirs = sorted(
+							d for d in os.listdir(self.root)
+							if os.path.isdir(os.path.join(self.root, d))
+					)
+		self.class_names = class_dirs
+		self.class_to_idx = {cls: i for i, cls in enumerate(self.class_names)}
 
-    # this won't validate the data
-    print(f"Found {len(self.class_names)} classes, {len(self.samples)} samples")
+		self.samples = []
+		for cls in self.class_names:
+			split_dir = os.path.join(self.root, cls, self.split)
+			off_files = glob.glob(os.path.join(split_dir, "*.off"))
+			for fpath in off_files:
+				self.samples.append((fpath, self.class_to_idx[cls]))
 
-  def __len__(self) -> int:
-    return len(self.samples)
-  
-  def __getitem__(self, index):
-    path, label_idx = self.samples[index]
-    points = sample_off_surface(path=path, n_points=self.num_points)
-    if self.normalize:
-      points = normalize_point_cloud(points=points)
-    
-    points = torch.from_numpy(points)
-    label = torch.tensor(label_idx)
+		# this won't validate the data
+		print(f"Found {len(self.class_names)} classes, {len(self.samples)} samples")
 
-    return points, label
+	def __len__(self) -> int:
+		return len(self.samples)
+	
+	def __getitem__(self, index):
+		path, label_idx = self.samples[index]
+		#print(self.cache_mode + self.precomputed_root)
+
+		use_cache = (
+			self.precomputed_root is not None
+			and self.cache_mode in ("write", "load")
+		)
+		
+		if use_cache:
+			# keep class/split folder structure under precomputed_root
+			# e.g. original: root/chair/train/chair_0001.off
+			# precomputed: precomputed_root/chair/train/chair_0001.npy
+			rel_path = os.path.relpath(path, self.root)
+			npy_path = os.path.splitext(os.path.join(self.precomputed_root, rel_path))[0] + ".npy"
+
+			if os.path.exists(npy_path) and (self.cache_mode == "load"):
+				# load mode
+				points = np.load(npy_path)
+			elif self.cache_mode == "load":
+				# attempt to load but no file path
+				raise FileNotFoundError(
+					f"Precomputed point cloud not found in 'load' mode: {npy_path}"
+				)
+			else:
+				# 'write' mode: compute and save, allow rewrite
+				print('write mode')
+				points = sample_off_surface(path=path, n_points=self.num_points)
+				if self.normalize:
+					points = normalize_point_cloud(points=points)
+
+				os.makedirs(os.path.dirname(npy_path), exist_ok=True)
+				np.save(npy_path, points)
+				print(f'saved {npy_path}')
+		else:
+			# dont use cache (sample the points on the fly)
+			points = sample_off_surface(path=path, n_points=self.num_points)
+			if self.normalize:
+				points = normalize_point_cloud(points=points)
+
+		points = torch.from_numpy(points.astype(np.float32))
+		label = torch.tensor(label_idx, dtype=torch.long)
+
+		return points, label
